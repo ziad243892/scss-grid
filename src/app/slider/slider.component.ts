1bf31e2bd673fe9fc66c8aa9240f94b54cbbeb7c
Ziad Abuzayyad <ziad_24389@hotmail.com>
2024-06-22 17:28:44 +0300

    css

--- src/app/slider/slider.component.ts
+++ src/app/slider/slider.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import {MatTabsModule} from '@angular/material/tabs';
+import { StarRatingComponent } from '../star-rating/star-rating.component';
 
 @Component({
   selector: 'app-slider',
   standalone: true,
-  imports: [MatTabsModule],
+  imports: [MatTabsModule, StarRatingComponent],
   templateUrl: './slider.component.html',
   styleUrl: './slider.component.scss'
 })
---
+++ src/app/star-rating/star-rating.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  standalone: true,
+  imports: [],
+  templateUrl: './star-rating.component.html',
+  styleUrl: './star-rating.component.scss'
+})
+export class StarRatingComponent implements OnInit {
+  constructor() {
+
+
+  }
+  ngOnInit(): void {
+
+  }
+
+}
